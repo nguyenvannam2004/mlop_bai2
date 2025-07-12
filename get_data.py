4d1b197a1dfd105a5781b4bc2acbd79a2e69a27f
nguyenvannam2004 <151933018+nguyenvannam2004@users.noreply.github.com>
2025-07-12 16:41:06 +0700

    Update data get_data.py

--- get_data.py
+++ get_data.py
@@ -1,4 +1,4 @@
 import pandas as pd
-url = "https://raw.githubusercontent.com/sharmaroshan/Heart-UCI-Dataset/master/heart.csv"
+url = "https://gist.githubusercontent.com/trantuyen082001/1fc2f5c0ad1507f40e721e6d18b34138/raw/heart.csv"
 df = pd.read_csv(url)
 df.to_csv("data/heart.csv", index=False)
